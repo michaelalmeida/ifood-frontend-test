29ba17d9f5eaa03b2d63bd312da1c5390714ecf4
Michael Almeida <michaelralmeida@gmail.com>
2020-07-28 18:51:00 -0300

    FilterManager

--- src/components/FilterBar/FilterManager.js
+++ src/components/FilterBar/FilterManager.js
@@ -1,7 +1,35 @@
 import PropTypes from 'prop-types';
 
+const FILTER_TYPE = Object.freeze({
+    validation: 'validation',
+    values: 'values',
+    number: 'INTEGER',
+    text: 'STRING',
+    datetime: 'DATE_TIME',
+});
+
 const FilterManager = ({ filterList }) => {
-    return console.log(filterList);
+    const hasProperty = (object, property) => {
+        return Object.prototype.hasOwnProperty.call(object, property);
+    };
+    return filterList.map((filter) => {
+        if (hasProperty(filter, FILTER_TYPE.values)) {
+            return console.log('select');
+        }
+        if (hasProperty(filter, FILTER_TYPE.validation)) {
+            if (filter.validation.primitiveType === FILTER_TYPE.number) {
+                return console.log('number');
+            }
+            if (filter.validation.primitiveType === FILTER_TYPE.text) {
+                if (filter.validation.entityType === FILTER_TYPE.datetime) {
+                    return console.log('date');
+                }
+                return console.log('text');
+            }
+        }
+
+        return console.log('nada');
+    });
 };
 
 FilterManager.propTypes = {
